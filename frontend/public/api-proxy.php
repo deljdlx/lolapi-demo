05ab15dc331639ed3916d39965a534db914d254b
Julien - Biniou <julien.delsescaux@oclock.io>
2020-06-07 11:18:26 +0200

    synchro

--- frontend/public/api-proxy.php
+++ frontend/public/api-proxy.php
@@ -5,92 +5,78 @@
 
 class LolApiProxy
 {
-    private $apiKey =  'RGAPI-1b4f370f-e921-4431-b785-63729a908a89';
+    private $apiKey = 'RGAPI-173102c1-99e6-4625-8797-c0b142c10cb7';
     private $apiRootUrl = 'https://euw1.api.riotgames.com/lol';
     private $dataRootUrl = 'http://ddragon.leagueoflegends.com/cdn/10.11.1/data/fr_FR';
 
     public function query($endpoint, $method = 'get', $data = [])
     {
-        if(strpos($endpoint, '/data') === 0 ) {
+        if (strpos($endpoint, '/data') === 0) {
             $endpoint = str_replace('/data', '', $endpoint);
             $url = $this->dataRootUrl . $endpoint;
+        } else {
+            $url = $this->apiRootUrl . $endpoint . '?api_key=' . $this->apiKey;
         }
-        else {
-            $url = $this->apiRootUrl . $endpoint . '?api_key='  . $this->apiKey;
-        }
-        
-        $data = $this->httpQuery($url , $method, $data);
+
+        $data = $this->httpQuery($url, $method, $data);
         return $data;
     }
 
+    public function httpQuery($url, $method = 'get', $data = [], $headers = [])
+    {
+        $method = strtoupper($method);
 
+        $headerString = '';
+        $contentTypeHeader = false;
 
-    public function httpQuery($url, $method='get', $data=array(), $headers=array()) {
-
-        $method=strtoupper($method);
-    
-        $headerString='';
-        $contentTypeHeader=false;
-        
-        foreach ($headers as $name=>$value) {
-            if($name=='Content-type') {
-                $contentTypeHeader=true;
+        foreach ($headers as $name => $value) {
+            if ($name == 'Content-type') {
+                $contentTypeHeader = true;
             }
-            $headerString.=$name.': '.$value."\r\n";
+            $headerString .= $name . ': ' . $value . "\r\n";
         }
-        
-        if(!$contentTypeHeader && $method=='POST') {
-            $headerString='Content-type: application/x-www-form-urlencoded'."\r\n".$headerString;
+
+        if (!$contentTypeHeader && $method == 'POST') {
+            $headerString = 'Content-type: application/x-www-form-urlencoded' . "\r\n" . $headerString;
         }
-    
-    
-        $raw=http_build_query($data);
-        
-        $options = array(
-            'http' => array(
-                'header'  => $headerString."Content-Length: ".strlen($raw)."\r\n",
-                'method'  => $method,
+
+        $raw = http_build_query($data);
+
+        $options = [
+            'http' => [
+                'header' => $headerString . 'Content-Length: ' . strlen($raw) . "\r\n",
+                'method' => $method,
                 'content' => $raw,
                 'request_fulluri' => true
-            ),
-        );
-        $context  = stream_context_create($options);
+            ],
+        ];
+        $context = stream_context_create($options);
         return file_get_contents($url, false, $context);
     }
-
 }
-
-
-
 
 $endpoint = $_GET['endpoint'];
 
-
 $cachePath = __DIR__ . '/cache/' . dirname($endpoint);
-$cacheFile = __DIR__.'/cache/' . $endpoint;
+$cacheFile = __DIR__ . '/cache/' . $endpoint;
 
-if(!is_dir($cachePath)) {
+if (!is_dir($cachePath)) {
     mkdir($cachePath, 0775, true);
 }
 
-
-if(is_file($cacheFile)) {
+if (is_file($cacheFile)) {
     $data = json_decode(file_get_contents($cacheFile));
-    if(!$data) {
+    if (!$data) {
         unlink($cacheFile);
     }
 }
 
 //$cacheFile = preg_replace('`\W`', '-', $endpoint);
-if(!is_file($cacheFile)) {
+if (!is_file($cacheFile)) {
     $proxy = new LolApiProxy();
     $content = $proxy->query($endpoint);
     file_put_contents($cacheFile, $content);
 }
 
-
 //header('Content-type: application/json');
 echo file_get_contents($cacheFile);
-
-
-
